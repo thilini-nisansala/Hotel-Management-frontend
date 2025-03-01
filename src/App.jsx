fc3bf3a44ae86d3deaea936188454d1f1ebbf074
Thilini Nisansala <thilininisansala953@gmail.com>
2025-03-02 01:13:07 +0530

    create HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
 
 import './App.css';
 import Header from './components/header';
+import HomePage from './pages/client-page/homepage';
 
 function App() {
 
 
   return (
     <>
-     <Header/>
-     <div className="w-full h-screen bg-blue-900 flex ">
-
-      <div className=" border border-white bg-white h-[100px] w-[700px] rounded-lg flex justify-center items-center">
-      
-        <input className='' type ="date"/>
-        <input type ="date"/>
-    
-    <select>
-      <option>Luxury</option>
-      <option>Normal</option>
-      <option>Low</option>
-    </select>
-    <button>Book Now</button>
-      
-      </div>
-
-     </div>
+    <HomePage/>
     </>
   );
 } 
---
+++ src/pages/client-page/homePage.jsx
@@ -0,0 +1,27 @@
+import Header from "../../components/header"
+export default function HomePage(){
+    return(
+    <>
+     <Header/>
+     <div className="w-full h-screen bg-blue-900 flex flex-col items-center">
+
+      <div className=" border border-white bg-white h-[100px] w-[700px] rounded-lg flex justify-center items-center">
+      
+        <input className='' type ="date"/>
+        <input type ="date"/>
+    
+    <select>
+      <option>Luxury</option>
+      <option>Normal</option>
+      <option>Low</option>
+    </select>
+    <button>Book Now</button> 
+      </div>
+
+    <h1 className='text-white text-[50px]'>
+      Welcome to the leonine villa
+    </h1>
+     </div>
+    </>
+    ) 
+}
