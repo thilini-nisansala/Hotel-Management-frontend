c88faad5c2b134c921be03a19453cc14bdcc0b3c
Thilini Nisansala <thilininisansala953@gmail.com>
2025-03-02 23:41:01 +0530

    alignment and add react icons

--- src/pages/adminpage/admin.jsx
+++ src/pages/adminpage/admin.jsx
@@ -1,42 +1,34 @@
 import { Link, Route, Routes } from "react-router-dom";
+import { CiBookmarkCheck } from "react-icons/ci";
+import { MdOutlineCategory, MdOutlineRoom, MdPeopleOutline, MdFeedback, MdPhotoLibrary } from "react-icons/md";
 
-export default function AdminPage(){
-
-    return(
+export default function AdminPage() {
+    return (
         <div className="w-full max-h-height-[100vh] overflow-hidden flex">
-
             <div className="w-[20%] bg-blue-400 h-[100vh] flex flex-col">
-
-                <div className="text-white text-[30px] hover:text-[60px] hover:text-black">
-                    <Link to="/admin/bookings">Bookings</Link>
-                </div>
-
-                <div className="text-white text-[30px] hover:text-[60px] hover:text-black">
-                <Link to="/admin/bookings">Categories</Link>
-                </div>
-
-                <div className="text-white text-[30px] hover:text-[60px] hover:text-black">
-                    <Link to="/admin/bookings">Rooms</Link>
-                </div>
-
-                <div className="text-white text-[30px] hover:text-[60px] hover:text-black">
-                    <Link to="/admin/bookings">Users</Link>
-                </div>
-
-                <div className="text-white text-[30px] hover:text-[60px] hover:text-black">
-                    <Link to="/admin/bookings">Feedback</Link>
-                </div>
-
-                <div className="text-white text-[30px] hover:text-[60px] hover:text-black">
-                    <Link to="/admin/bookings">Gallary Items</Link>
-                </div>
-
+                <Link to="/admin/bookings" className="text-white text-[30px] hover:font-bold flex items-center gap-2">
+                    <CiBookmarkCheck /> Bookings
+                </Link>
+                <Link to="/admin/categories" className="text-white text-[30px] hover:font-bold flex items-center gap-2">
+                    <MdOutlineCategory /> Categories
+                </Link>
+                <Link to="/admin/rooms" className="text-white text-[30px] hover:font-bold flex items-center gap-2">
+                    <MdOutlineRoom /> Rooms
+                </Link>
+                <Link to="/admin/users" className="text-white text-[30px] hover:font-bold flex items-center gap-2">
+                    <MdPeopleOutline /> Users
+                </Link>
+                <Link to="/admin/feedback" className="text-white text-[30px] hover:font-bold flex items-center gap-2">
+                    <MdFeedback /> Feedback
+                </Link>
+                <Link to="/admin/gallery" className="text-white text-[30px] hover:font-bold flex items-center gap-2">
+                    <MdPhotoLibrary /> Gallery Items
+                </Link>
             </div>
 
-        <div className="w-[80%] bg-blue-900">
-
+            <div className="w-[80%] bg-blue-900">
+                {/* Admin page content goes here */}
+            </div>
         </div>
-
-        </div>
-    )
+    );
 }
