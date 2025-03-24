5c946f3b2e9496a4f68c016749646cffddb79cda
Thilini Nisansala <thilininisansala953@gmail.com>
2025-03-25 00:16:12 +0530

    create login page

--- src/components/userData.jsx
+++ src/components/userData.jsx
@@ -1,7 +1,26 @@
+import axios from "axios";
+
 
 
 function UserTag(props){
    
+    const token =localStorage.getItem("token")
+    
+
+    if(token != null){
+        console.log(token)
+        axios.get(import.meta.env.VITE_BACKEND_URL+ "/api/users/",
+        
+       {
+            headers:{
+                Authorization : "Bearer " +token,
+                "Content-Type" : "application/json"
+            }
+        }).then((res)=>{
+            console.log(res)
+        })
+    }
+
     return(
         <div className="absolute right-0 flex items-center cursor-pointer mr-2">
 
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,46 +1,69 @@
 import { useState } from "react";
 import axios from "axios";
 import "./login.css";
-
 export default function LoginPage(){
 
-    const [email,setEmail] = useState(" ")
-    const [password, setPassword] =useState("")
+  const [email ,setEmail] = useState("")
+  const [password , setPassword] = useState("")
 
-    function handleLogin(){
-        axios.post("http://localhost:5000/api/users/login",
-        {
-            email : email,
-            password : password
-        }).then((res)=>{
-            console.log(res.data)
-            localStorage.setItem("token",res.data.token)
+  function handleLogin(){
 
-            const token = localStorage.getItem("token")
+    axios.post(import.meta.env.VITE_BACKEND_URL+"/api/users/login",
+      {
+        email : email,
+        password : password
+      }
+    ).then(
+      (res)=>{
 
-            console.log(token)
+      localStorage.setItem("token",res.data.token)
 
-            if(res.data.user.type == "admin"){
-                window.location.href = "/admin"
-            }else{
-                window.location.href = "/"
-            }
+      const token=localStorage.getItem("token")
+      
+      console.log(res.data.user.type)
 
-        }).catch((err)=>{
-            console.log(err)
-        })
-    }
+      if(res.data.user.type == "customer"){
+        window.location.href = "/"
+      }else if(res.data.user.type == "admin"){
+        window.location.href = "/admin"
+      }
+      
 
-    return(
-        <div className="w-full h-[100vh] pic-bg flex justify-center items-center">
-                <div className="w-[400px] h-[400px] backdrop-blur-md rounded-lg flex flex-col items-center justify-center relative">
-                <h1 className="text-3xl p-[15px] text-white absolute top-[40px] text-center">Login</h1>
+    }).catch((err)=>{
 
-                <input type="text" placeholder="Enter your email address" className="w-[80%] bg-[#00000000] text-white placeholder:text-white  border-white border-[2px] h-[40px] px-[5px] mb-[20px]" defaultValue={email} onChange={(e)=>{setEmail(e.target.value)}}/>
-                <input type="password" placeholder="Enter your password" className="w-[80%]  bg-[#00000000] text-white placeholder:text-white  border-white border-[2px] h-[40px] px-[5px]  "  defaultValue={password} onChange={(e)=>{setPassword(e.target.value)}}/>
-                <button className="w-[80%] absolute bottom-[40px] bg-red-500 text-white h-[50px]" onClick={handleLogin} >Login</button>
-                </div>
+      console.log(err)
 
-            </div>
-    )
-} 
+    })
+
+  }
+
+  return(
+    <div className="w-full h-[100vh]  pic-bg  flex justify-center items-center">
+      <div className="w-[400px] h-[400px] backdrop-blur-md  rounded-lg flex flex-col items-center justify-center relative">
+        <h1 className="text-3xl p-[15px] text-white absolute top-[40px] text-center">Login</h1>
+
+
+
+        <input type="text" placeholder="Enter your email address" className="w-[80%] bg-[#00000000]  border-[2px]  text-white placeholder:text-white h-[50px] px-[5px] mb-[20px] " 
+        defaultValue={email} 
+        onChange={
+
+          (e)=>{
+            setEmail(e.target.value)
+          }
+
+        } />
+
+
+        <input type="password" placeholder="Enter your password" className="w-[80%] bg-[#00000000]  border-[2px]  text-white placeholder:text-white h-[50px] px-[5px] " defaultValue={password} onChange={
+          (e)=>{
+            setPassword(e.target.value)
+          }
+        }/>
+
+        <button className="w-[80%] absolute bottom-[40px] bg-red-500 text-white h-[50px] " onClick={handleLogin}>Login</button>
+
+      </div>
+    </div>
+  )
+}
